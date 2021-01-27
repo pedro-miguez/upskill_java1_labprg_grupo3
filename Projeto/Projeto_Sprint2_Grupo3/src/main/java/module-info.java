c727295d4cc90b564a05e720a2b6403bf4e3c810
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-01-27 09:25:06 +0000

    criação projecto esqueleto

---
+++ Projeto/Projeto_Sprint2_Grupo3/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.projeto_sprint2_grupo3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.projeto_sprint2_grupo3 to javafx.fxml;
+    exports com.mycompany.projeto_sprint2_grupo3;
+}
+*/
