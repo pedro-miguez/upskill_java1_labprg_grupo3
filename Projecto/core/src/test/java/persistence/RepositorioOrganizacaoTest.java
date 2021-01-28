6a1b218265ac219be690e6e6993d6e90223e4cb9
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-01-28 15:57:38 +0000

    TDD RepositorioOrganizacao

--- Projecto/core/src/test/java/persistence/RepositorioOrganizacaoTest.java
+++ Projecto/core/src/test/java/persistence/RepositorioOrganizacaoTest.java
@@ -1,6 +1,57 @@
 package persistence;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RepositorioOrganizacaoTest {
 
+    @Test
+    public void testNovaOrganizacaoValida() {
+
+    }
+
+    @Test
+    public void testNovaOrganizacaoInvalida() {
+
+    }
+
+    @Test
+    public void testValidaOrganizacaoValida() {
+
+    }
+
+    @Test
+    public void testValidaOrganizacaoInvalida() {
+
+    }
+
+    @Test
+    public void testRegistaOrganizacaoValida() {
+
+    }
+
+    @Test
+    public void testRegistaOrganizacaoInvalida() {
+
+    }
+
+    @Test
+    public void testAddOrganizacaoValida() {
+
+    }
+
+    @Test
+    public void testAddOrganizacaoInvalida() {
+
+    }
+
+    @Test
+    public void testRegistaGestorComoUtilizadorValido() {
+
+    }
+
+    @Test
+    public void testRegistaGestorComoUtilizadorInvalido() {
+
+    }
 }
