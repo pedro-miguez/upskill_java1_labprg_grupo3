5001f0bcd9ac4dbd622f2727ff9cf7601ecb359b
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-01-28 16:46:55 +0000

    Update RepositorioOrganizacao.java

--- Projecto/core/src/main/java/persistence/RepositorioOrganizacao.java
+++ Projecto/core/src/main/java/persistence/RepositorioOrganizacao.java
@@ -1,15 +1,42 @@
 package persistence;
 
+import domain.Colaborador;
 import domain.Organizacao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RepositorioOrganizacao {
 
+    private static RepositorioOrganizacao instance;
+
+    private RepositorioOrganizacao(){
+        organizacoesRegistadas = new ArrayList<Organizacao>();
+    }
+
     private List<Organizacao> organizacoesRegistadas;
-    
-    //metodos vazios
-    //so funcionalidades e testes posteriormente ou vice versa
-    //listaOrganizacoes
-    
+
+    public static RepositorioOrganizacao getInstance(){
+        if(instance == null){
+            instance = new RepositorioOrganizacao();
+        }
+        return instance;
+    }
+
+    public void addOrganizacao(Organizacao organizacao) {
+        this.organizacoesRegistadas.add(organizacao);
+    }
+
+    public Organizacao getOrganizacaoByGestor(Colaborador colaborador) {
+        for (Organizacao o : organizacoesRegistadas) {
+            if (o.getGestor().equals(colaborador)) {
+                return o;
+            }
+        }
+        return null; //throw new GestorNaoRelacionadoANenhumaOrganizacaoException()
+    }
+
+    public ArrayList<Organizacao> listarOrganizacoes() {
+        return new ArrayList<Organizacao>(this.organizacoesRegistadas);
+    }
 }
