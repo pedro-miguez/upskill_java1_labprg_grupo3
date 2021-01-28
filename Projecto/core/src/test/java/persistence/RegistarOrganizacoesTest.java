858ea6c0533761d7660e29bd29aa01b8ce6589c8
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-01-28 10:42:32 +0000

    criação do projecto

---
+++ Projecto/core/src/test/java/persistence/RegistarOrganizacoesTest.java
@@ -0,0 +1,7 @@
+package persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistarOrganizacoesTest {
+
+}
--- Projecto/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module grupo_3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens grupo_3 to javafx.fxml;
-    exports grupo_3;
-}
-*/
