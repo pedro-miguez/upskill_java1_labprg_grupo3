a5da1544e7ea9be24f3660dcadd873e617699f75
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-01-29 18:07:38 +0000

    update

---
+++ Projecto/core/src/main/java/ui/JanelaInicialUI.java
@@ -0,0 +1,33 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class JanelaInicialUI implements Initializable {
+
+    public Button login;
+    public Button registar;
+
+    public void GoToLogin(ActionEvent actionEvent) {
+        //Acesso_login.fxml
+        
+        
+        
+    }
+
+    public void RegistarOrganizacao(ActionEvent actionEvent) {
+        //registar_Organizacao.fxml
+        
+        
+        
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
