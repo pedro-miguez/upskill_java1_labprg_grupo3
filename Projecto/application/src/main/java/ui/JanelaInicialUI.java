953149071ced4c246a4279e7568a865ffef7ee9a
anibalfarraia2020 <74573559+anibalfarraia2020@users.noreply.github.com>
2021-01-29 17:41:55 +0000

    update

--- Projecto/application/src/main/java/ui/JanelaInicialUI.java
+++ Projecto/application/src/main/java/ui/JanelaInicialUI.java
@@ -1,16 +1,34 @@
 package ui;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class JanelaInicialUI {
+public class JanelaInicialUI implements Initializable {
 
     public Button login;
     public Button registar;
 
     public void GoToLogin(ActionEvent actionEvent) {
+        //Acesso_login.fxml
+        
+        
+        
     }
 
     public void RegistarOrganizacao(ActionEvent actionEvent) {
+        //registar_Organizacao.fxml
+        
+        
+        
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
