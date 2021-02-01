e8d188a09d7481715fe5cd413e70db03ca1de7ea
juliofssa <juliofs.sa@upskill.pt>
2021-02-01 17:34:15 +0000

    Update AreaAdministrador

--- Projecto/core/src/main/java/ui/AreaAdministradorUI.java
+++ Projecto/core/src/main/java/ui/AreaAdministradorUI.java
@@ -1,4 +1,61 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+
 public class AreaAdministradorUI {
+
+    @FXML
+    private Button limparBtn;
+
+    @FXML
+    private TextField txtDescBreve;
+
+    @FXML
+    private Button voltarBtn;
+
+    @FXML
+    private Button confirmarBtn;
+
+    @FXML
+    private TextArea txtDescDetalhada;
+
+    @FXML
+    private TextField txtCodUnico;
+
+    @FXML
+    void voltarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limparAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void criarAreaAtividadeAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void criarCategoriaTarefaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void criarCompetenciaTecnicaAction(ActionEvent event) {
+
+    }
+
 }
