e5fed211dcce2cb2239d3d9cadd244aafa120a6e
juliofssa <juliofs.sa@upskill.pt>
2021-02-01 15:37:53 +0000

    Update DailyScrum

--- Projecto/core/src/main/java/application/DefinirAreaAtividadeController.java
+++ Projecto/core/src/main/java/application/DefinirAreaAtividadeController.java
@@ -1,4 +1,16 @@
 package application;
 
+import domain.AreaAtividade;
+import domain.CodigoUnico;
+import persistence.RepositorioAreaAtividade;
+
 public class DefinirAreaAtividadeController {
+
+    public boolean definirAreaAtividade(String codigoUnico, String descricao, String descricaoDetalhada) {
+
+        AreaAtividade areaAtividade = new AreaAtividade(new CodigoUnico(codigoUnico), descricao, descricaoDetalhada);
+
+        return RepositorioAreaAtividade.getInstance().addAreaAtividade(areaAtividade);
+    }
+
 }
