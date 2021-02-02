c53ab621de3635ceaf45dd714a1eb949412c1116
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-02-02 10:28:12 +0000

    update UC1 last stretch

--- Projecto/core/src/main/java/ui/RegistarOrganizacaoUI.java
+++ Projecto/core/src/main/java/ui/RegistarOrganizacaoUI.java
@@ -1,5 +1,6 @@
 package ui;
 
+import application.RegistarOrganizacaoController;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,69 +11,62 @@ import javafx.stage.Stage;
 
 public class RegistarOrganizacaoUI extends Application {
 
-    @FXML
-    private TextField Localidade_Org;
+    public TextField txtNomeGestor;
+    public TextField txtContactoGestor;
+    public TextField txtEmailGestor;
+    public TextField txtFuncaoGestor;
+    public TextField txtNomeOrg;
+    public TextField txtNIFOrg;
+    public TextField txtMoradaOrg;
+    public TextField txtLocalidadeOrg;
+    public TextField txtCodigoPostalOrg;
+    public TextField txtTelefoneOrg;
+    public TextField txtWebsiteOrg;
+    public TextField txtEmailOrg;
+    public Button btnVoltar;
+    public Button limparDados;
+    public Button registarOrganizacao;
 
-    @FXML
-    private TextField website;
-
-    @FXML
-    private TextField FuncaoGestor;
-
-    @FXML
-    private Button limparDados;
-
-    @FXML
-    private TextField NIF_Org;
-
-    @FXML
-    private Button Send_Info;
-
-    @FXML
-    private TextField Tlf_Org;
-
-    @FXML
-    private TextField NomeGestor;
-
-    @FXML
-    private TextField ContactoGestor;
-
-    @FXML
-    private TextField Nome_Org;
-
-    @FXML
-    private TextField Email_Org;
-
-    @FXML
-    private Button voltar;
-
-    @FXML
-    private TextField CP_Org;
-
-    @FXML
-    private TextField Morada_Org;
-
-    @FXML
-    private TextField EmailGestor;
-
-    @FXML
-    void Sending_Info_Org(ActionEvent event) {
-
-    }
-
-    @FXML
-    void Clear_Info(ActionEvent event) {
-
-    }
-
-    @FXML
-    void GotoMain(ActionEvent event) {
-        MainApp.screenController.activate("JanelaInicial");
-    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+    }
+
+    public void registarOrganizacaoAction(ActionEvent actionEvent) {
+        try {
+            RegistarOrganizacaoController controller = new RegistarOrganizacaoController();
+
+            boolean added = controller.registarOrganizacao(
+                    txtNomeOrg.getText().trim(),
+                    Integer.parseInt(txtNIFOrg.getText()),
+                    txtWebsiteOrg.getText().trim(),
+                    Integer.parseInt(txtTelefoneOrg.getText()),
+                    txtEmailOrg.getText().trim(),
+                    );
+            if (adicionou) {
+                janelaPrincipalUI.atualizaTextAreaListaTelefonica();
+            }
+
+            AlertaUI.criarAlerta(Alert.AlertType.INFORMATION, MainApp.TITULO_APLICACAO, "Adicionar novo contacto.",
+                    adicionou ? "Contacto adicionado com sucesso."
+                            : "Não foi possível adicionar o contacto.").show();
+
+            encerrarNovoContactoUI(event);
+        } catch (NumberFormatException nfe) {
+            AlertaUI.criarAlerta(Alert.AlertType.ERROR, MainApp.TITULO_APLICACAO, "Erro nos dados.",
+                    "Introduza um valor numérico para telefone!").show();
+        } catch (IllegalArgumentException iae) {
+            AlertaUI.criarAlerta(Alert.AlertType.ERROR, MainApp.TITULO_APLICACAO, "Erro nos dados.",
+                    iae.getMessage()).show();
+        }
+    }
+
+    public void limparAction(ActionEvent actionEvent) {
+    }
+
+    public void voltarAction(ActionEvent actionEvent) {
+        MainApp.screenController.activate("JanelaInicial");
     }
 }
 
