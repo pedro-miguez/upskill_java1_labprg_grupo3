9830d7e5653ad4a24292b3b551ed63c90c71ab7b
juliofssa <juliofs.sa@upskill.pt>
2021-02-02 16:48:20 +0000

    Create TestRepositorioTarefa, Tarefa

---
+++ Projecto/core/src/test/java/persistence/RepositorioTarefaTest.java
@@ -0,0 +1,34 @@
+package persistence;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RepositorioTarefaTest {
+
+    @Test
+    public void testGetInstance() {
+
+    }
+
+    @Test
+    public void addTarefaValida(){
+
+    }
+
+    @Test
+    public void addTarefaDuplicada(){
+
+    }
+
+    @Test
+    public void getTarefaByReferenciaValida(){
+
+    }
+
+    @Test
+    public void getTarefaByReferenciaInvalida(){
+
+    }
+
+}
