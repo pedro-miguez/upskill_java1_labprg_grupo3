fd3f8cbe35f88865501cdf7512d257aa3242047e
Vitor Luis <victor88_6@upskill.pt>
2021-02-04 15:23:06 +0000

    updated javadoc

--- Projecto/core/src/test/java/domain/CaracterizacaoCompTecTest.java
+++ Projecto/core/src/test/java/domain/CaracterizacaoCompTecTest.java
@@ -19,7 +19,7 @@ public class CaracterizacaoCompTecTest {
 
         GrauProficiencia grauProficiencia = GrauProficiencia.ALTO;
 
-        ***************************************************************
+        //***************************************************************
         //new caracterizacao comp test
 
         String expected = "Código Unico: JAV-01; Area de Actividade: Código Unico: PRG-01; Descrição: Programação de Jogos; " +
