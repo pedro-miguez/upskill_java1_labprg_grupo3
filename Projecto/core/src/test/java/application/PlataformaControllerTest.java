e979881e9a211fd8d3d3eed84233785835ea2b2c
Vitor Luis <victor88_6@upskill.pt>
2021-02-05 12:45:46 +0000

    Update PlataformaControllerTest.java

--- Projecto/core/src/test/java/application/PlataformaControllerTest.java
+++ Projecto/core/src/test/java/application/PlataformaControllerTest.java
@@ -1,9 +1,10 @@
 package application;
 
-import domain.AreaAtividade;
-import domain.CodigoUnico;
-import domain.Plataforma;
+import domain.*;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +22,31 @@ public class PlataformaControllerTest {
         assertEquals(expected, result);
     }
 
+    /*@Test
+    public void testGetCategoriasTarefa(){
+
+        CodigoUnico codigoUnico = new CodigoUnico("PRG-01");
+        AreaAtividade areaAtividade = new AreaAtividade(codigoUnico, "Programacao de Jogos", "Programacao de variados jogos em JAVA");
+        List<CaracterizacaoCompTec> competenciasTecnicas = new ArrayList<>();
+        DefinirCategoriaTarefaController controller = new DefinirCategoriaTarefaController();
+        controller.definirCategoriaTarefa(areaAtividade, "Canalizacao", competenciasTecnicas);
+
+        CategoriaTarefa catTar = new CategoriaTarefa(areaAtividade, "Programacao",competenciasTecnicas);
+
+
+        List<CategoriaTarefa> ct = new ArrayList<>();
+                ct.add(0,catTar);
+
+        Plataforma.getInstance().getRepoCategoriaTarefa().listarCategoriasTarefa();
+        PlataformaController c = new PlataformaController();
+
+        assertTrue(controller.getCategoriasTarefa(catTar));
+
+        criar catTar
+                adicionar
+                        get
+
+    }*/
 
 
 }
