c614725325774029b6c1bf44fea6e7629299a098
anibalfarraia2020 <74573559+anibalfarraia2020@users.noreply.github.com>
2021-02-17 17:43:13 +0000

    Update Anuncio.java
    update

--- Projecto/core/src/main/java/domain/Anuncio.java
+++ Projecto/core/src/main/java/domain/Anuncio.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
  */
 public class Anuncio implements Serializable {
     
-    private int idAnuncio;
+    //private int idAnuncio;
     
     private Tarefa idTarefa;
     
@@ -23,6 +23,54 @@ public class Anuncio implements Serializable {
     private Data dataFimCandidatura;
     private Data dataInicioSeriacao;
     private Data dataFimSeriacao;
+    
+    
+    public Anuncio (Tarefa idTarefa, Data dataInicioPublicitacao,
+                    Data dataFimPublicitacao, Data dataInicioCandidatura,
+                    Data dataFimCandidatura, Data dataInicioSeriacao, Data dataFimSeriacao) {
+        
+        //setIdAnuncio(idAnuncio);
+        
+        this.idTarefa = idTarefa;
+        
+        this.dataInicioPublicitacao = dataInicioPublicitacao;
+        this.dataFimPublicitacao = dataFimPublicitacao;
+        this.dataInicioCandidatura = dataInicioCandidatura;
+        this.dataFimCandidatura = dataFimCandidatura;
+        this.dataInicioSeriacao = dataInicioSeriacao;
+        this.dataFimSeriacao = dataFimSeriacao;
+        
+    }
+    
+    
+    public Tarefa getIdTarefa() {
+        return this.idTarefa;
+    }
+    
+    public Data getDataInicioPublicitacao() {
+        return this.dataInicioPublicitacao;
+    }
+    
+    public Data getDataFimPublicitacao() {
+        return this.dataFimPublicitacao;
+    }
+    
+    public Data getDataInicioCandidatura() {
+        return this.dataInicioCandidatura;
+    }
+    
+    public Data getDataFimCandidatura() {
+        return this.dataFimCandidatura;
+    }
+    
+    public Data getDataInicioSeriacao() {
+        return this.dataInicioSeriacao;
+    }
+    
+    public Data getDataFimSeriacao() {
+        return this.dataFimSeriacao;
+    }
+    
     
     
     
