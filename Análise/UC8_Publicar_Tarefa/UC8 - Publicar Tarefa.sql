ceba2e5e83ef1617a4bc5b38987322def13bdf47
anibalfarraia2020 <74573559+anibalfarraia2020@users.noreply.github.com>
2021-02-19 15:57:59 +0000

    Update UC8 - Publicar Tarefa.sql
    update

--- Análise/UC8_Publicar_Tarefa/UC8 - Publicar Tarefa.sql
+++ Análise/UC8_Publicar_Tarefa/UC8 - Publicar Tarefa.sql
@@ -1,1 +1,16 @@
-UC8 - Publicar Tarefa
+create or replace procedure createAnuncio( 
+
+    p_idTarefa tarefa.IdTarefa%type,
+    p_dataInicioPub anuncio.dataInicioPublicitacao%type,
+    p_dataFimPub anuncio.dataFimPublicitacao%type,
+    p_dataInicioCand anuncio.dataInicioCandidatura%type,
+    p_dataFimCand anuncio.dataFimCandidatura%type,
+    p_dataInicioSer anuncio.dataInicioSeriacao%type,
+    p_dataFimSer anuncio.dataFimSeriacao%type) 
+    is
+    begin 
+insert into Anuncio(IdTarefa, dataInicioPublicitacao, dataFimPublicitacao, dataInicioCandidatura, dataFimCandidatura,
+                    dataInicioSeriacao, dataFimSeriacao) values (p_idTarefa, p_dataInicioPub, p_dataFimPub,
+                    p_dataInicioCand, p_dataFimCand, p_dataInicioSer, p_dataFimSer); 
+end;
+/
