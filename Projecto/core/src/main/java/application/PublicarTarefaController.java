40fae5b6a197a536b30e1e0e7de75ca01051a4cf
anibalfarraia2020 <74573559+anibalfarraia2020@users.noreply.github.com>
2021-02-22 10:18:36 +0000

    Create PublicarTarefaController.java
    new class

---
+++ Projecto/core/src/main/java/application/PublicarTarefaController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import domain.*;
+import persistence.RepositorioOrganizacao;
+import persistence.RepositorioTarefa;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Grupo 3
+ */
+public class PublicarTarefaController {
+    
+    public boolean publicarTarefa() throws SQLException {
+        
+        
+        
+    }
+    
+}
