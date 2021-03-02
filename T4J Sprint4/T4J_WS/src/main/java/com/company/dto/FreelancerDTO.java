cc2cad82bb7b9b600b612e5e850de11cdb6865a7
anibalfarraia2020 <74573559+anibalfarraia2020@users.noreply.github.com>
2021-03-02 15:58:27 +0000

    dto
    updates

--- T4J Sprint4/T4J_WS/src/main/java/com/company/dto/FreelancerDTO.java
+++ T4J Sprint4/T4J_WS/src/main/java/com/company/dto/FreelancerDTO.java
@@ -5,10 +5,51 @@
  */
 package com.company.dto;
 
+import com.company.model.Email;
+import com.company.model.Telefone;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JsonPropertyOrder({"nif", "nome"})
+@JacksonXmlRootElement(localName = "freelancer")
+
 /**
  *
  * @author Grupo 3
  */
 public class FreelancerDTO {
+    @JacksonXmlProperty(localName = "nif")
+    private long nif;
+    @JacksonXmlProperty(localName = "nome")
+    private String nome;
     
+
+    public FreelancerDTO() {
+        
+    }
+
+    public long getNif() {
+        return nif;
+    }
+
+    public void setNif(long nif) {
+        this.nif = nif;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    void setTelefone(Telefone telefone) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setEmail(Email email) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
