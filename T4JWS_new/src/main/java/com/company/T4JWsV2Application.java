1840d15fc56318fc289a98d653cb4e9e95c4295c
anibalfarraia2020 <74573559+anibalfarraia2020@users.noreply.github.com>
2021-03-03 15:41:32 +0000

    T4J_WS_new
    simple version

---
+++ T4JWS_new/src/main/java/com/company/T4JWsV2Application.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T4JWsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T4JWsV2Application.class, args);
+	}
+
+}
