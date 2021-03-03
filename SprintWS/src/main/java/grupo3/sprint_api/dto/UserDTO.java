6c7ebb60ee08640f9284c9ab946fdfbe0f8b13a0
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-03-03 10:44:11 +0000

    update web service project

---
+++ SprintWS/src/main/java/grupo3/sprint_api/dto/UserDTO.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo3.sprint_api.dto;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JsonPropertyOrder({"username", "password", "email", "role"})
+@JacksonXmlRootElement(localName = "user")
+
+/**
+ *
+ * @author Grupo 3
+ */
+public class UserDTO {
+    @JacksonXmlProperty(localName = "username")
+    private String username;
+    @JacksonXmlProperty(localName = "password")
+    private String password;
+    @JacksonXmlProperty(localName = "email")
+    private String email;
+    @JacksonXmlProperty(localName = "role")
+    private String role;
+    
+
+    public UserDTO() {
+        super();
+    }
+
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
