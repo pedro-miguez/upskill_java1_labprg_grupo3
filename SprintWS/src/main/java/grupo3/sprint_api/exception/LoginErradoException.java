47fc06e7201922c05c0511eb40c5122023670df8
Vitor Luis <victor88_6@upskill.pt>
2021-03-03 17:18:09 +0000

    update UsersService
    Co-Authored-By: Júlio Sá <74595961+juliofssa@users.noreply.github.com>
    Co-Authored-By: Pedro Vieira Miguez <74610206+pedro-miguez@users.noreply.github.com>
    Co-Authored-By: anibalfarraia2020 <74573559+anibalfarraia2020@users.noreply.github.com>

---
+++ SprintWS/src/main/java/grupo3/sprint_api/exception/LoginErradoException.java
@@ -0,0 +1,10 @@
+package grupo3.sprint_api.exception;
+
+/**
+ *
+ * @author Grupo 3 - Turma 1
+ */
+public class LoginErradoException extends IllegalArgumentException {
+    public LoginErradoException(String s) {
+        super(s);
+    }
