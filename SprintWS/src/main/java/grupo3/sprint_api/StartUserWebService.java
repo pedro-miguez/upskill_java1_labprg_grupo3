fa816e5130ccb3e39f27de339a6fd2742894df71
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-03-03 10:36:08 +0000

    update

---
+++ SprintWS/src/main/java/grupo3/sprint_api/StartUserWebService.java
@@ -0,0 +1,13 @@
+package grupo3.sprint_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUserWebService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUserWebService.class, args);
+	}
+
+}
---
+++ SprintWS/src/test/java/grupo3/sprint_api/SprintApiApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo3.sprint_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
