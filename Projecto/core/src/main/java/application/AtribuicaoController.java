293b615868817e6b1d927da4dedc0168ad5416d0
anibalfarraia2020 <74573559+anibalfarraia2020@users.noreply.github.com>
2021-03-16 14:38:14 +0000

    Update AtribuicaoController.java

--- Projecto/core/src/main/java/application/AtribuicaoController.java
+++ Projecto/core/src/main/java/application/AtribuicaoController.java
@@ -5,12 +5,39 @@
  */
 package application;
 
+import domain.*;
+import persistence.RepositorioCandidatura;
+import persistence.RepositorioColaborador;
+import persistence.RepositorioAtribuicao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author Grupo 3
  */
 public class AtribuicaoController {
     
+    public boolean isAtribuicaoObrigatoria (Anuncio anuncio) {
+        
+        return anuncio.isSeriacaoAutomatica();
+        
+    }
     
+    public boolean criarProcessoAtribuicao (List<Candidatura> candidaturas,
+                                         List<Colaborador> colaboradores, 
+                                         String emailColaborador)
+                                                        throws SQLException {
+        
+        colaboradores.add(RepositorioColaborador.getInstance().getColaboradorByEmail(new Email(emailColaborador)));
+        
+        Atribuicao processoAtribuicao = RepositorioAtribuicao.getInstance().criarProcessoAtribuicao();
+        
+        return RepositorioAtribuicao.getInstance().insertProcessoAtribuicao(processoAtribuicao);
+        
+    }
     
 }
