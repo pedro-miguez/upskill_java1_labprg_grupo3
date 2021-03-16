f0cbb09e28d360a0a40389ea2f26abc89916e29e
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-03-16 10:54:43 +0000

    updates TipoRegimento

---
+++ Projecto/core/src/main/java/domain/SeriacaoManualObrigatoria.java
@@ -0,0 +1,85 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Current class implements the tools for creating new type of regiment.
+ * 
+ * @author Grupo 3
+ */
+public class SeriacaoManualObrigatoria implements TipoRegimento {
+
+    private String designacao;
+    private String regras;
+
+    /**
+     * Instantiates a new type of regiment.
+     * 
+     * @param designacao
+     * @param regras 
+     */
+    public SeriacaoManualObrigatoria(String designacao, String regras) {
+        this.designacao = designacao;
+        this.regras = regras;
+    }
+
+    @Override
+    public ArrayList<Classificacao> seriarCandidaturas(List<Candidatura> candidaturas) {
+        return null;
+    }
+
+    @Override
+    public Atribuicao atribuirSeriacao(ProcessoSeriacao processoSeriacao) {
+        return null;
+    }
+
+    /**
+     * Gets the designation.
+     * 
+     * @return designacao
+     */
+    public String getDesignacao() {
+        return designacao;
+    }
+
+    /**
+     * Sets the designation.
+     * 
+     * @param descricao 
+     */
+    public void setDesignacao(String descricao) {
+        this.designacao = descricao;
+    }
+
+    /**
+     * Gets the rules.
+     * 
+     * @return regras
+     */
+    public String getRegras() {
+        return regras;
+    }
+
+    /**
+     * Sets the rules.
+     * 
+     * @param regras 
+     */
+    public void setRegras(String regras) {
+        this.regras = regras;
+    }
+
+    
+    /**
+     * Returns a string representation with very concise but precise information 
+     * about the object and its attributes.
+     *
+     * @return a string representation of the object (type of regiment).
+     *
+     */
+    @Override
+    public String toString() {
+        return designacao;
+    }
+}
